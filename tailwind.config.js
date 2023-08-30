97d4ce75968a087414b274af107c6cfb8e82f263
isaacrumbold <isaacrumbold@gmail.com>
2023-08-30 14:15:49 -0500

    setup: config default text and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    'Roboto',
+                    'Helvetica',
+                    'sans-serif',
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+        },
+    },
+    plugins: [],
+}
