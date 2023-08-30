4b3657f4e918160f64640d194afca4091f0d3ba8
isaacrumbold <isaacrumbold@gmail.com>
2023-08-30 13:43:51 -0500

    styled the html element

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
-        <>
-            <div className="ml-4 flex bg-slate-400 p-5">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs ">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+          <div className="flex h-full w-full items-center bg-slate-400 justify-center">
+            <h1>Hello There</h1>
+          </div>
     )
 }
 
