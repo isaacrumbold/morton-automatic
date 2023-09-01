a33bb8589aa93f4912ae6f95df4eb047c5f0b8d5
isaacrumbold <isaacrumbold@gmail.com>
2023-09-01 08:49:05 -0500

    fix: deleted old browserrouter and added new one to make routes accessible

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,15 +3,18 @@ import { Home } from './routes/Home'
 import { Layout } from './routes/Layout'
 import { About } from './routes/About'
 import { Contact } from './routes/Contact'
+import { BrowserRouter } from 'react-router-dom'
 
 function Root() {
     return (
-        <Routes>
-            <Route path="/" element={<Layout />} />
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />} />
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './Root.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import ErrorPage from './error-page.tsx'
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />,
-        errorElement: <ErrorPage />,
-    },
-])
+import Root from './Root'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Root />
     </React.StrictMode>
 )
