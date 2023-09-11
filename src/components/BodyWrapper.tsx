b2acbf57421d4e622e281c043ef8413e4bc7eef7
isaacrumbold <isaacrumbold@gmail.com>
2023-09-11 13:49:13 -0500

    fix: centered div

--- src/components/BodyWrapper.tsx
+++ src/components/BodyWrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export const BodyWrapper = ({ children }: PropsWithChildren) => {
     return (
-        <div className="w-full bg-white">
+        <div className="flex w-full justify-center bg-white">
             <div className="mx-16 flex max-w-6xl max-lg:flex-col">
                 {children}
             </div>
