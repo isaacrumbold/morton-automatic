4aca1132bd61cc660180c1c801766ac138c24c1c
isaacrumbold <isaacrumbold@gmail.com>
2023-09-14 14:36:53 -0500

    fix: added function to close menu when a link is selected

--- src/components/Ref.tsx
+++ src/components/Ref.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 type RefProps = {
     linkTo: string
     name: string
+    closeMenu?: () => void
 }
-export const Ref = ({ linkTo, name }: RefProps) => {
+export const Ref = ({ linkTo, name, closeMenu }: RefProps) => {
     return (
         <Link
             className=" mx-2 my-1 flex h-full items-center border-b-4 border-white text-lg transition duration-500 hover:border-primary"
             to={linkTo}
+            onClick={closeMenu}
         >
             {name}
         </Link>
