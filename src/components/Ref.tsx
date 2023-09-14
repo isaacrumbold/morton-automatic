82e6ae72652a591a463a0b2c36ce8771aec352df
isaacrumbold <isaacrumbold@gmail.com>
2023-09-14 14:51:15 -0500

    fix: fixed border on large display

--- src/components/Ref.tsx
+++ src/components/Ref.tsx
@@ -8,7 +8,7 @@ type RefProps = {
 export const Ref = ({ linkTo, name, closeMenu }: RefProps) => {
     return (
         <Link
-            className=" mx-2 my-1 flex h-full items-center border-b-4 border-white text-lg transition duration-500 hover:border-primary"
+            className=" mx-2 flex h-full items-center border-b-4 border-white text-lg transition duration-500 hover:border-primary max-lg:my-1"
             to={linkTo}
             onClick={closeMenu}
         >
