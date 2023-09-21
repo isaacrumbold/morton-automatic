54d653e55dd6b5672d53f59f8a7e3c3af2a3267d
Isaac Rumbold <116579756+isaacrumbold@users.noreply.github.com>
2023-09-21 13:42:27 -0500

    Revert "feat: added portfolio route and link"
    This reverts commit 64aa5aa38a765903e7e9d808a33f94d55eaa6868.

--- src/Root.tsx
+++ src/Root.tsx
@@ -7,7 +7,6 @@ import { NotFound } from './routes/NotFound'
 import { NavBar } from './components/NavBar'
 import { Footer } from './components/Footer'
 import { useEffect } from 'react'
-import { Portfolio } from './routes/Portfolio'
 
 function Root() {
     const { pathname } = useLocation()
@@ -22,7 +21,6 @@ function Root() {
                 <Route index element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="contact" element={<Contact />} />
-                <Route path="portfolio" element={<Portfolio />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
