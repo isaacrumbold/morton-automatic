ed7d7f0f27fe989017bfe34e7b3b43ce88cb4ecd
Isaac Rumbold <116579756+isaacrumbold@users.noreply.github.com>
2023-09-21 13:42:27 -0500

    Revert "feat: added Banner comp to home, and changed logic to allow for break in title text"
    This reverts commit 89569ab96bf1d230105ddfd03fdbe413135033a9.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,11 @@
 import home_banner from './images/home_banner_filtered.jpg'
 import { BodyPara } from './components/BodyPara'
 import { BodyWrapper } from '../components/BodyWrapper'
-import { Banner } from './components/Banner'
 
 export const Home = () => {
     return (
         <div className="flex h-fit w-full flex-col items-center bg-white">
-            {/* <div className="absolute z-20 flex h-[1000px] w-full -translate-y-52 overflow-hidden">
+            <div className="absolute z-20 flex h-[1000px] w-full -translate-y-52 overflow-hidden">
                 <img
                     src={home_banner}
                     alt="home banner"
@@ -20,15 +19,7 @@ export const Home = () => {
                 </div>
 
                 <div className=" absolute bottom-0 flex h-96 w-full translate-y-44 skew-y-2 items-center justify-center bg-white"></div>
-            </div> */}
-            <Banner
-                title1="Creating Innovative"
-                title2="Solutions Since"
-                subtitle="1972"
-                image={home_banner}
-                alt="home banner"
-                top={360}
-            />
+            </div>
 
             <div className="z-20 mt-[550px] flex h-max w-3/4 min-w-mobile max-w-5xl flex-col justify-center rounded-3xl bg-white drop-shadow-xl">
                 <div className=" mx-3 my-5">
