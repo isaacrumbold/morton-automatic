8e49f44bd735937412d6afd1f5dc8f0b80a4a616
Isaac Rumbold <116579756+isaacrumbold@users.noreply.github.com>
2023-09-21 13:42:27 -0500

    Revert "feat: added Banner comp to home, and changed logic to allow for break in title text"
    This reverts commit 89569ab96bf1d230105ddfd03fdbe413135033a9.

--- src/routes/components/Banner.tsx
+++ src/routes/components/Banner.tsx
@@ -1,31 +1,19 @@
 type BannerProps = {
-    title1: string
-    title2?: string
+    title: string
     subtitle: string
     image: string
     top: number
     alt: string
 }
 
-export const Banner = ({
-    title1,
-    title2,
-    subtitle,
-    image,
-    top,
-    alt,
-}: BannerProps) => {
+export const Banner = ({ title, subtitle, image, top, alt }: BannerProps) => {
     return (
         <div className="absolute z-20 flex h-[1000px] w-full -translate-y-52 overflow-hidden">
             <img src={image} alt={alt} className="w-full object-cover" />
             <div
                 className={`absolute top-[${top}px] flex h-fit w-full flex-col bg-transparent  text-center  text-white`}
             >
-                <h1 className=" text-5xl font-medium leading-tight">
-                    {title1}
-                    {title2 && <br />}
-                    {title2}
-                </h1>
+                <h1 className=" text-5xl font-medium leading-tight">{title}</h1>
                 <h2 className="text-4xl font-light leading-tight">
                     {subtitle}
                 </h2>
