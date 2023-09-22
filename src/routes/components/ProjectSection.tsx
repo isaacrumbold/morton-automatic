6c01c30ca52f49b005b9a69158f04a55777e503c
isaacrumbold <isaacrumbold@gmail.com>
2023-09-22 13:55:51 -0500

    feat: WORK IN PROGRESS - adding scrollable card for images

--- src/routes/components/ProjectSection.tsx
+++ src/routes/components/ProjectSection.tsx
@@ -1,4 +1,6 @@
 import { ChevronRightIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
+import { ProjectCard } from './ProjectCard'
 
 type ProjectSectionProps = {
     image: string
@@ -9,6 +11,11 @@ type ProjectSectionProps = {
     flipped?: boolean
 }
 
+type IsOpenType = {
+    open: boolean
+    css: 'hidden' | 'block'
+}
+
 export const ProjectSection = ({
     image,
     title,
@@ -16,33 +23,72 @@ export const ProjectSection = ({
     alt,
     flipped = false,
 }: ProjectSectionProps) => {
+    const [isOpen, setIsOpen] = useState<IsOpenType>({
+        open: false,
+        css: 'hidden',
+    })
     const flipRow = flipped ? 'flex-row-reverse' : 'flex-row'
+
+    const toggleOpen = () => {
+        isOpen.open
+            ? setIsOpen({ open: false, css: 'hidden' })
+            : setIsOpen({ open: true, css: 'block' })
+    }
     return (
-        <>
+        <div className="z-20 mb-16 flex h-fit flex-col items-center max-lg:mb-24">
             <div
-                className={`z-20 flex w-full max-w-6xl max-lg:flex-col ${flipRow} mb-16 max-lg:mb-24 max-lg:items-center`}
+                className={` flex w-full max-w-6xl max-lg:flex-col ${flipRow}  max-lg:items-center`}
             >
-                <div className="m-4 mb-6 h-fit min-w-mobile max-lg:mb-2 max-lg:w-4/6 lg:w-1/2">
+                <div className="m-4 mb-6 h-fit min-w-mobile max-lg:mx-5 max-lg:mb-2 max-lg:w-auto lg:w-1/2">
                     <img
                         src={image}
                         alt={alt}
                         className=" w-full rounded-md object-cover"
                     />
                 </div>
-                <div className="mx-4 my-8 flex min-w-mobile flex-col justify-center overflow-hidden max-lg:my-4 max-lg:w-4/6 lg:w-1/2">
+                <div className="mx-6 my-8 flex min-w-mobile flex-col justify-center overflow-hidden max-lg:my-4 max-lg:w-auto lg:w-1/2">
                     <h1 className="mb-3 text-2xl font-bold text-primary">
                         {title}
                     </h1>
                     <p className=" text-gray-700">{description}</p>
                     <button
-                        className=" group mt-6 w-36 rounded-md border-2 border-primary py-1 text-lg text-primary transition-all duration-500  hover:w-44 hover:cursor-pointer hover:bg-primary hover:text-white"
-                        disabled={true}
+                        className={`group mt-6 w-36 rounded-md border-2 border-primary py-1 text-lg text-primary transition-all duration-500  hover:w-44 hover:cursor-pointer hover:bg-primary hover:text-white ${
+                            isOpen.open ? 'w-44 bg-primary text-white' : ''
+                        }`}
+                        onClick={toggleOpen}
                     >
-                        Learn more
-                        <ChevronRightIcon className="ml-2 inline-block h-5 text-primary opacity-0 transition-all delay-200  duration-300 group-hover:text-white group-hover:opacity-100" />
+                        Examples
+                        <ChevronRightIcon
+                            className={`ml-2 inline-block h-5 text-primary  transition-all  duration-300   ${
+                                isOpen.open
+                                    ? 'rotate-90 text-white'
+                                    : 'opacity-0 delay-200 group-hover:text-white group-hover:opacity-100'
+                            }`}
+                        />
                     </button>
                 </div>
             </div>
-        </>
+            <div
+                className={`flex h-fit w-11/12 max-w-6xl flex-col justify-center border-l-4 border-primary ${isOpen.css} `}
+            >
+                {/* option 1 */}
+                <ProjectCard />
+                {/* option 2 */}
+                <div className=" mx-10 flex w-5/6 text-primary">
+                    <div className=" h-fit w-1/2 max-lg:mb-2">
+                        <img
+                            src={image}
+                            alt={alt}
+                            className=" w-full rounded-md object-cover"
+                        />
+                    </div>
+                    <div className=" mx-1 flex w-1/2  flex-col justify-end">
+                        <h1 className="mb-3 flex h-3/4 items-center rounded-e-3xl border-y-4 border-r-4 border-primary px-4 text-xl font-bold">
+                            CAT Control Panel for Things
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
