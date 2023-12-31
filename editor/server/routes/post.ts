8be91b5a266f7bfbb06d63e8daa06c7d9ad002f7
isaacrumbold <isaacrumbold@gmail.com>
2023-12-31 11:24:29 -0600

    wip

--- editor/server/routes/post.ts
+++ editor/server/routes/post.ts
@@ -2,17 +2,13 @@ import { writeFile } from "fs";
 import { Request, Response } from "express";
 
 export const createProjectOrExample = (req: Request, res: Response, filePath: string | undefined) => {
-    try {
-        writeFile(
-            `.${filePath}/json/projects.json`,
-            JSON.stringify(req.body), (err: any) => {
-                if (err) throw err
-                console.log("file successfully written")
-            }
-        )
-        res.status(200).json(req.body)
-    } catch
-    (err) {
-        console.error("File Error:\n", err)
-    }
+    writeFile(
+        `.${filePath}/json/projects.json`,
+        JSON.stringify(req.body), (err: any) => {
+            if (err) throw err
+            console.log("file successfully written")
+        }
+    )
+    res.status(200).json(req.body)
 }
+
