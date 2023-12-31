878ababaf1ac5cb41bab833f44e0fd66315cf567
isaacrumbold <isaacrumbold@gmail.com>
2023-12-31 11:40:20 -0600

    added back delete file that is used in server.ts

---
+++ editor/server/routes/delete.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { rmSync } from "fs";
+
+
+export const deleteDirectory = (req: Request, res: Response, isProduction: boolean, filePath: string | undefined) => {
+    req.body.idArray.forEach((id: number) => {
+        if (filePath) throw new Error(`file path is of type ${typeof filePath}`)
+        rmSync(
+            isProduction
+                ? `.${filePath}/portfolioImages/${id}`
+                : `.${filePath}/public/portfolioImages/${id}`,
+            { recursive: true, force: true },
+        )
+    })
+}
